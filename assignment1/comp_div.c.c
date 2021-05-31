c3807291b79a78471af1eb460cb43ab9cae94781
AryansRathi <85131054+AryansRathi@users.noreply.github.com>
2021-05-31 21:00:17 +0200

    Update comp_div.c.c

--- assignment1/comp_div.c.c
+++ assignment1/comp_div.c.c
@@ -11,5 +11,3 @@ result = (3.0 + 1.0) / 5.0;
 printf("The value of 4/5 is %lf\n", result);
 return 0;
 }
-//The result was coming 0.000 as the we wanted a double result but the numbers did not have decimal notations that why the result was 0.000//
-https://www.xvideos.com/video60556909/painful_punjabi_pussy_fucking_mms_video
